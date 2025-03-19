1cf530ea6e336ea42cd185c1706bdd0d932b524d
maiwandddd <1011409@pdsb.net>
2025-03-19 10:34:28 -0400

    Header

---
+++ codeing_challenge/02_loop/loop_01.py
@@ -0,0 +1,19 @@
+#-----------------------------------------------------------------------------
+# Name:        Maiwand Alkozai
+# Purpose:     to guess the right number
+#
+# Author:      Maiwand Alkozai
+# Created:     18-Mar-2025
+# Updated:     19-Mar-2025
+#-----------------------------------------------------------------------------
+import random
+
+random_number = random.randint(1, 10)
+myNumber = int(input("Guess the number from 1 to 10: "))
+
+if myNumber == random_number:
+    print("You win!")
+else:
+    print(f"Try again, the correct number was {random_number}.")
+
+print(f"The random number is: {random_number}")
