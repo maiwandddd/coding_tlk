9dc84f543810f184419179b88e7dc2bfc9256d50
maiwandddd <1011409@pdsb.net>
2025-03-21 10:06:59 -0400

    header

---
+++ Loops_challenges/challenges/03_loops/03_loops/loop_03.py
@@ -0,0 +1,12 @@
+#-----------------------------------------------------------------------------
+# Name:        Maiwand Alkozai
+# Purpose:     to skip the even numbers
+#
+# Author:      Maiwand Alkozai
+# Created:     19-Mar-2025
+# Updated:     20-Mar-2025
+#-----------------------------------------------------------------------------
+for num in range(1, 10, 2):
+    print(num)
+if num % 2 == 2:
+    print("only odd numbers")
